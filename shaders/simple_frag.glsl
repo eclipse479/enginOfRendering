d9806515feb62c7fafeaafd9c644f2ddc7533fc6
aaron <aaronzstv@gmail.com>
2020-02-26 10:50:24 +1100

    texture lighting checkpoint

--- shaders/simple_frag.glsl
+++ shaders/simple_frag.glsl
@@ -1,28 +1,41 @@
 #version 450 // Frag Shader
 
-uniform sampler2D diffuse_texture; 		//FOR TEXTURE
-uniform vec3 lightDirection; 			// FOR LIGHTING
-uniform vec3 ambientLight; 				// FOR LIGHTING
-uniform vec3 diffuseLight; 				// FOR LIGHTING
-uniform vec3 specularLight; 			// FOR LIGHTING
+uniform sampler2D textureDiffuse; 		//FOR TEXTURE
+uniform sampler2D textureSpecular;		//FOR TEXTURE
+uniform sampler2D textureNormal;		//FOR TEXTURE
+uniform vec3 lightDirection; 			//FOR LIGHTING
+uniform vec3 ambientLight; 				//FOR LIGHTING
+uniform vec3 diffuseLight; 				//FOR LIGHTING
+uniform vec3 specularLight; 			//FOR LIGHTING
 
-uniform vec3 Ka; 						// FOR LIGHTING
-uniform vec3 Kd; 						// FOR LIGHTING
-uniform vec3 Ks; 						// FOR LIGHTING
-uniform float specularPower = 10; 		// FOR LIGHTING
+uniform vec3 Ka; 						//FOR LIGHTING
+uniform vec3 Kd; 						//FOR LIGHTING
+uniform vec3 Ks; 						//FOR LIGHTING
+uniform float specularPower = 10; 		//FOR LIGHTING
 
-uniform vec3 cameraPosition; 			// FOR LIGHTING
-in vec4 vPosition;						// FOR LIGHTING
-in vec3 vNormal; 						// FOR LIGHTING
-in vec2 final_texture_coodinates; 		//	FOR TEXTURE
+uniform vec3 cameraPosition; 			//FOR LIGHTING
+in vec4 vPosition;						//FOR LIGHTING
+in vec3 vNormal; 						//FOR LIGHTING
+in vec2 final_texture_coodinates; 		//FOR TEXTURE
+
+in vec3 vTangent;
+in vec3 vBiTangent;
 
 out vec4 final_color;
 
 void main()
 {
 	//make sure normal and light direction is normalised
-	vec3 N = normalize(vNormal); 										// FOR LIGHTING
+	vec3 N = normalize(vNormal); 	// FOR LIGHTING
+	vec3 T = normalize(vTangent);	
+	vec3 B = normalize(vBiTangent);
 	vec3 L = normalize(lightDirection); 								// FOR LIGHTING
+	
+	mat3 TBN =  mat3(T,B,N);
+	
+	vec3 texDifuse = texture(textureDiffuse,final_texture_coodinates).rgb;
+	vec3 texSpecular = texture(textureSpecular,final_texture_coodinates).rgb;
+	vec3 texNormal = texture(textureNormal, final_texture_coodinates).rgb;
 	
 	float lambertTerm = max(0,min(1,dot(N,-L))); 						// FOR LIGHTING
 	
@@ -31,11 +44,11 @@ void main()
 	// calculate specular term
 	float specularTerm = pow( max( 0, dot( R, V ) ), specularPower ); 	// FOR LIGHTING
 	
-    vec4 textureColour = texture(diffuse_texture, final_texture_coodinates);
+    vec4 textureColour = texture(textureDiffuse, final_texture_coodinates);
 	vec3 colourToAdd = vec3 (textureColour.x,textureColour.y,textureColour.z);
 	vec3 ambient = ambientLight * Ka; 									// FOR LIGHTING
 	vec3 diffuse = (diffuseLight * Kd * lambertTerm * colourToAdd); 	// FOR LIGHTING
 	vec3 specular = specularLight * Ks * specularTerm; 					// FOR LIGHTING
-	//final_color = texture(diffuse_texture, final_texture_coodinates); // FOR TEXTURE
+	//final_color = texture(textureDiffuse, final_texture_coodinates); // FOR TEXTURE
 	final_color = vec4(ambient + diffuse + specular, 1.0); 				// FOR LIGHTING
 }
--- shaders/simple_vertex.glsl
+++ shaders/simple_vertex.glsl
@@ -2,11 +2,13 @@
 layout(location = 0) in vec3 local_position;
 layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 texture_coordinates;
-
+layout(location = 3) in vec4 tangent;
 
 out vec2 final_texture_coodinates;					//FOR TEXTURE
-out vec4 vPosition;
+out vec4 vPosition;									//FOR LIGHTING
 out vec3 vNormal; 									//FOR LIGHTING
+out vec3 vTangent;
+out vec3 vBiTangent;
 
 uniform mat4 projection_view_matrix;
 uniform mat4 model_matrix;
@@ -14,8 +16,10 @@ uniform mat3 normalMatrix; 							//FOR LIGHTING
 
 void main()
 {
-	//vPosition = model_matrix * local_position;
+	final_texture_coodinates = texture_coordinates; //	FOR TEXTURE
+	vPosition = model_matrix * vec4(local_position,1);
     vNormal = normalMatrix * normal.xyz; 			// FOR LIGHTING
-    final_texture_coodinates = texture_coordinates; //	FOR TEXTURE
+vTangent = normalMatrix * tangent.xyz;
+vBiTangent = cross(vNormal, vTangent) * tangent.w;
     gl_Position = (projection_view_matrix * model_matrix) * vec4(local_position, 1);
 }
